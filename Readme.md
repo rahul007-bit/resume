9924f2ab58dded145b918d7dc0817cecc132e7bf
rahul007-bit <yadavrahul8141@yahoo.com>
2023-10-02 11:29:30 +0530

    add readme version 1.1

---
+++ Readme.md
@@ -0,0 +1,13 @@
+<!-- insert image here from Images/image.pmg -->
+
+# Screenshots
+<p align="center">
+    <img src="Images/image-1.png" width="100%" height="100%" alt="Screenshot">
+    <img src="Images/image-2.png" width="100%" height="100%" alt="Screenshot">
+</p>
+
+# Author
+**Rahul Yadav**
+
+* linkedIn: [@rahulyadav8141](https://www.linkedin.com/in/rahulyadav8141/)
+* Github: [@rahul007-bit](https://www.github.com/rahul007-bit)
---
+++ getimage.sh
@@ -0,0 +1,18 @@
+# convert pdf to png with gs
+# Usage: getimage.sh <pdf file>
+
+FILEPATH=$(cd "$(dirname "$1")"; pwd)/$(basename "$1")
+
+# check file exist
+if [ ! -f "$FILEPATH" ]; then
+    echo "File not found!"
+    exit 1
+fi
+
+command -v gs >/dev/null 2>&1 || { echo >&2 "Ghostscript is not installed.  Aborting."; exit 1; }
+
+run() {
+    gs -dSAFER -dBATCH -dNOPAUSE -sDEVICE=png16m -r300 -dGraphicsAlphaBits=4 -dTextAlphaBits=4 -sOutputFile="image-%d.png" "$1"
+}
+
+run "$FILEPATH"
